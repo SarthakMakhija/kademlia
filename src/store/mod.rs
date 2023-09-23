97a078e9b0d15f5d131dddf41d014c72acacaa0a
Sarthak <sarthak.makhija@gmail.com>
2023-09-23 14:49:10 +0530

    Sarthak | Refactors Store, moves Key and StoredValue in the same mod.rs

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,14 +1,49 @@
-mod key;
-mod stored_value;
-
 use std::collections::HashMap;
-use crate::store::key::Key;
-use crate::store::stored_value::StoredValue;
+
+pub(crate) const EXPECTED_KEY_ID_LENGTH: usize = 20;
+
+pub(crate) type KeyId = Vec<u8>;
+
+pub(crate) struct Key {
+    pub(crate) id: KeyId,
+    pub(crate) key: Vec<u8>,
+}
+
+impl Key {
+    pub(crate) fn new(id: Vec<u8>, key: Vec<u8>) -> Self {
+        if id.len() != EXPECTED_KEY_ID_LENGTH {
+            let error_message = format!("key id length must be {}, but was {}", EXPECTED_KEY_ID_LENGTH, id.len());
+            panic!("{}", error_message.as_str())
+        }
+        Key {
+            id,
+            key
+        }
+    }
+}
 
 pub(crate) trait Store {
     fn put_or_update(&mut self, key: Key, value: Vec<u8>);
     fn get(&self, key: &Vec<u8>) -> Option<&Vec<u8>>;
     fn delete(&mut self, key: &Vec<u8>);
+}
+
+pub(crate) struct StoredValue {
+    pub(crate) key_id: KeyId,
+    pub(crate) value: Vec<u8>,
+}
+
+impl StoredValue {
+    pub(crate) fn new(key_id: KeyId, value: Vec<u8>) -> Self {
+        StoredValue {
+            key_id,
+            value
+        }
+    }
+
+    pub(crate) fn value(&self) -> &Vec<u8> {
+        &self.value
+    }
 }
 
 pub(crate) struct InMemoryStore {
@@ -39,16 +74,34 @@ impl Store for InMemoryStore {
 
 #[cfg(test)]
 mod tests {
-    use crate::store::{InMemoryStore, Store};
-    use crate::store::key::Key;
+    use crate::store::{EXPECTED_KEY_ID_LENGTH, InMemoryStore, Key, Store};
 
+    #[test]
+    #[should_panic]
+    fn incorrect_key_id_length() {
+        let _key = Key::new(
+            vec![10],
+            vec![10, 20, 30],
+        );
+    }
+
+    #[test]
+    fn correct_key_id_length() {
+        let key = Key::new(
+            vec![0; 20],
+            vec![10, 20, 30],
+        );
+
+        assert_eq!(vec![0; 20], key.id);
+        assert_eq!(vec![10, 20, 30], key.key);
+    }
     #[test]
     fn get_the_value_for_the_existing_key() {
         let mut store = InMemoryStore::new_in_memory_store();
         let key = "kademlia".as_bytes().to_vec();
         let value = "distributed hash table".as_bytes().to_vec();
 
-        store.put_or_update(Key::new(vec![1; 20], key), value);
+        store.put_or_update(Key::new(vec![1; EXPECTED_KEY_ID_LENGTH], key), value);
 
         let query_key = "kademlia".as_bytes().to_vec();
         let stored_value = store.get(&query_key);
@@ -95,7 +148,7 @@ mod tests {
         let key = "kademlia".as_bytes().to_vec();
         let value = "distributed hash table".as_bytes().to_vec();
 
-        store.put_or_update(Key::new(vec![1; 20], key), value);
+        store.put_or_update(Key::new(vec![1; EXPECTED_KEY_ID_LENGTH], key), value);
 
         let key_to_delete = "kademlia".as_bytes().to_vec();
         store.delete(&key_to_delete);
