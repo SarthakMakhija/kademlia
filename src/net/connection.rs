78aa1fa1b0a195ac0114ae5cb34afe99a88bc252
Sarthak <sarthak.makhija@gmail.com>
2023-09-30 13:32:52 +0530

    Sarthak | Makes TcpConnection async

--- src/net/connection.rs
+++ src/net/connection.rs
@@ -1,46 +1,55 @@
-use std::io::{Error, Write};
-use std::net::TcpStream;
+use std::io::Error;
+
+use tokio::io;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+
 use crate::net::endpoint::Endpoint;
 
 pub(crate) struct TcpConnection {
-   tcp_stream: TcpStream
+    tcp_stream: TcpStream,
 }
 
 impl TcpConnection {
-    fn establish_with(endpoint: Endpoint) -> Result<TcpConnection, Error> {
+    async fn establish_with(endpoint: Endpoint) -> Result<TcpConnection, Error> {
         TcpStream::connect(endpoint.address())
+            .await
             .map(|tcp_stream| TcpConnection { tcp_stream })
     }
 
-    fn write(&mut self, payload: &[u8]) -> std::io::Result<usize> {
-        self.tcp_stream.write(payload)
+    async fn write(&mut self, payload: &[u8]) -> io::Result<()> {
+        self.tcp_stream.write_all(payload).await?;
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::net::TcpListener;
+    use tokio::net::TcpListener;
+
     use crate::net::connection::TcpConnection;
     use crate::net::endpoint::Endpoint;
 
-    #[test]
-    fn write_to_connect_successfully() {
-        let _listener = TcpListener::bind("127.0.0.1:9898").unwrap();
+    #[tokio::test]
+    async fn write_to_connect_successfully() {
+        let listener_result = TcpListener::bind("localhost:9898").await;
+        assert!(listener_result.is_ok());
 
-        let tcp_connection_result = TcpConnection::establish_with(Endpoint::new("127.0.0.1".to_string(), 9898));
+        let tcp_connection_result =
+            TcpConnection::establish_with(Endpoint::new("localhost".to_string(), 9898)).await;
         assert!(tcp_connection_result.is_ok());
 
         let mut tcp_connection = tcp_connection_result.unwrap();
         let payload = b"Kademlia";
-        let write_result = tcp_connection.write(payload);
 
+        let write_result = tcp_connection.write(payload).await;
         assert!(write_result.is_ok());
-        assert_eq!(payload.len(), write_result.unwrap())
     }
 
-    #[test]
-    fn connect_to_endpoint_fails() {
-        let tcp_connection_result = TcpConnection::establish_with(Endpoint::new("127.0.0.1".to_string(), 1010));
+    #[tokio::test]
+    async fn connect_to_endpoint_fails() {
+        let tcp_connection_result =
+            TcpConnection::establish_with(Endpoint::new("localhost".to_string(), 1010)).await;
         assert!(tcp_connection_result.is_err());
     }
 }
